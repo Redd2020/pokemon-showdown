10464c38f3791516458a3643f065529816ce11a9
Redd2020 <dab09267@gmail.com>
2022-10-24 16:38:47 -0500

    commit

--- data/text/abilities.ts
+++ data/text/abilities.ts
@@ -456,10 +456,10 @@ export const AbilitiesText: {[k: string]: AbilityText} = {
 	},
 	forecast: {
 		name: "Forecast",
-		desc: "If this Pokemon is a Castform, its type changes to the current weather condition's type, except Sandstorm. If this Pokemon is holding Utility Umbrella and the weather condition is Sunny Day, Desolate Land, Rain Dance, or Primordial Sea, it will not change types.",
-		shortDesc: "Castform's type changes to the current weather condition's type, except Sandstorm.",
+		desc: "Summons weather based on held weather rock, its type changes to the current weather condition's type, except Sandstorm. If this Pokemon is holding Utility Umbrella and the weather condition is Sunny Day, Desolate Land, Rain Dance, or Primordial Sea, it will not change types.",
+		shortDesc: "Summons weather based on held weather rock, type changes to the current weather condition's type, except Sandstorm.",
 		gen7: {
-			desc: "If this Pokemon is a Castform, its type changes to the current weather condition's type, except Sandstorm.",
+			desc: "Summons weather based on held weather rock, its type changes to the current weather condition's type, except Sandstorm.",
 		},
 	},
 	forewarn: {
